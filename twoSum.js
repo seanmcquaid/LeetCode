57465524dd3aac347a90f80d3949cad242221f7b
seanmcquaid <sean.mcquaid2@verizon.net>
2020-05-12 20:06:39 -0400

    initial commit

---
+++ twoSum.js
@@ -0,0 +1,14 @@
+const twoSum = (nums, target) => {
+    for(let i = 0; i < nums.length; i++){
+        for(let j = 0; j < nums.length; j++){
+            if(nums[i] + nums[j] === target && i !== j){
+                return [i, j]
+            }
+        }
+    }
+};
+
+const nums = [2, 7, 11, 15, 2];
+const target = 9;
+
+console.log(twoSum(nums, target));
