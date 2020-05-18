38a95c94567c1a71658b9e001b53f75df0628674
seanmcquaid <sean.mcquaid2@verizon.net>
2020-05-17 22:58:17 -0400

    wrote a basic solution for reverse integer in java, going to review this for the overflow edge cases

---
+++ ReverseInteger.java
@@ -0,0 +1,15 @@
+public class ReverseIntenger {
+    public static void main(String[] args){
+        System.out.println(reverse(123));
+    }
+
+    public static int reverse (int x){
+        int reversedNum = 0;
+        while(x != 0){
+            int digit = x % 10;
+            reversedNum = reversedNum * 10 + digit;
+            x /= 10;
+        }
+        return reversedNum;
+    }
+}
