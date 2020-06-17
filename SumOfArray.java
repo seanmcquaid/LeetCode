4630399d56dda8f9a69adc2d3d307ff83e7c4ee1
seanmcquaid <sean.mcquaid2@verizon.net>
2020-06-17 17:47:21 -0500

    sum of array solution in java

---
+++ SumOfArray.java
@@ -0,0 +1,11 @@
+public class SumOfArray {
+    public static int[] runningSum(int[] nums) {
+        int sum = 0;
+        int[] sumArray = new int[nums.length];
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            sumArray[i] = sum;
+        }
+        return sumArray;
+    }
+}
