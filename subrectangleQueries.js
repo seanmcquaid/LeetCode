fe10db23768023f671caf66a0015dcd84f65f98d
seanmcquaid <sean.mcquaid2@verizon.net>
2020-09-21 22:12:54 -0400

    wrote subrectangle queries solution

---
+++ subrectangleQueries.js
@@ -0,0 +1,17 @@
+class SubrectangleQueries {
+  constructor(rectangle) {
+    this.rectangle = rectangle;
+  }
+
+  updateSubrectangle(row1, col1, row2, col2, newValue) {
+    for (let i = row1; i <= row2; i++) {
+      for (let j = col1; j <= col2; j++) {
+        this.rectangle[i][j] = newValue;
+      }
+    }
+  }
+
+  getValue(row, col) {
+    return this.rectangle[row][col];
+  }
+}
